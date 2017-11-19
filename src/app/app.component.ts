4f07d4657ce4bdb3794f74724ed805014cffcc59
SATYANANDANA SRIKANTHVARMA VADAPALLI <srikanthvarma.vadapalli@gmail.com>
2017-11-19 13:30:29 -0700

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component ,OnInit, HostListener,ElementRef,HostBinding} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  @HostBinding() private title: String = 'app';
+
+  @HostListener('mouseover') onHover() {
+  window.alert('hover');
+}
+
+  constructor(private el: ElementRef){
+
+  }
+
+  ngOnInit(): void {
+    this.title = 'MyApp';
+  }
+
+}
